3a99dd3169529fc40f98b60a7cdb3e85bd9f0bb1
sarakrcelic <129505063+sarakrcelic@users.noreply.github.com>
2024-12-18 23:31:56 +0100

    Delete zavrsni_projekt_pzw/zavrsnipzw directory

--- zavrsni_projekt_pzw/zavrsnipzw/main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'main'  # here for namespacing of urls.
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('register/', views.register, name='register'),
-]
